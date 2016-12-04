fe008143922b86022f905613d0f37ecaf68b27a1
Mitchell Skaggs <skaggsm333@gmail.com>
2016-12-04 09:14:47 -0600

    Added Day 4 Problems 1 and 2

--- java/_2016/day4/Problem2.java
+++ java/_2016/day4/Problem2.java
@@ -12,16 +12,15 @@ import java.util.stream.Collectors;
 /**
  * Created by Mitchell on 12/4/2016.
  */
-public class Problem1 {
+public class Problem2 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(Problem1.class.getResourceAsStream("day4.txt"));
+        Scanner s = new Scanner(Problem2.class.getResourceAsStream("day4.txt"));
 
         List<String> lines = new ArrayList<>();
         while (s.hasNext())
             lines.add(s.next());
 
         List<String> correctRooms = new ArrayList<>();
-        int sum = 0;
         for (String l : lines) {
             Multiset<Character> letters = TreeMultiset.create();
             char[] chars = l.toCharArray();
@@ -40,11 +39,22 @@ public class Problem1 {
             //System.out.println(targets);
             //System.out.println(mostCommon);
             if (targets.equals(mostCommon)) {
-                String[] split2 = split[0].split("-");
-                sum += Integer.valueOf(split2[split2.length - 1]);
                 correctRooms.add(l);
             }
         }
-        System.out.println("Day 4 Problem 1: " + sum);
+
+        for (String r : correctRooms) {
+            String[] split = r.split("\\[")[0].split("-");
+            int shift = Integer.parseInt(split[split.length - 1]);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < split.length - 1; i++) {
+                for (char c : split[i].toCharArray()) {
+                    sb.append((char) (((c - 97 + shift) % 26) + 97));
+                }
+                sb.append(' ');
+            }
+            if (sb.toString().contains("north"))
+                System.out.println("Day 4 Problem 2: " + sb + " Shift: " + shift);
+        }
     }
 }
