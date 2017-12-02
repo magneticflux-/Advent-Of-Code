7f9d9882237e4f719d4ec2b7e30a4fcde5be5221
Mitchell Skaggs <skaggsm333@gmail.com>
2017-12-02 12:10:14 -0600

    Added Day 2 solutions

--- src/main/kotlin/com/github/magneticflux/aoc/day2/Problem1.kt
+++ src/main/kotlin/com/github/magneticflux/aoc/day2/Problem1.kt
@@ -1,10 +1,18 @@
 package com.github.magneticflux.aoc.day2
 
+import com.github.magneticflux.aoc.input
+import java.util.*
+import kotlin.math.absoluteValue
+
 /**
  * Created by Mitchell Skaggs on 12/1/2017.
  */
 object Problem1 {
     @JvmStatic
     fun main(args: Array<String>) {
+        val input = Scanner(input())
+        input.useDelimiter("\n")
+        val grid = input.asSequence().map { it.split('\t').map { it.toInt() }.sorted() }.toList()
+        println(grid.map { (it.first() - it.last()).absoluteValue }.sum())
     }
 }
--- src/main/kotlin/com/github/magneticflux/aoc/day2/Problem2.kt
+++ src/main/kotlin/com/github/magneticflux/aoc/day2/Problem2.kt
@@ -1,10 +1,30 @@
 package com.github.magneticflux.aoc.day2
 
+import com.github.magneticflux.aoc.input
+import java.util.*
+
 /**
  * Created by Mitchell Skaggs on 12/1/2017.
  */
 object Problem2 {
     @JvmStatic
     fun main(args: Array<String>) {
+        val input = Scanner(input())
+        input.useDelimiter("\n")
+        val grid = input.asSequence().map { it.split('\t').map { it.toInt() } }.toList()
+        println(grid.map {
+            var first: Int? = null
+            var second: Int? = null
+
+            it.forEach { a ->
+                it.forEach { b ->
+                    if (a % b == 0 && a != b) {
+                        first = a
+                        second = b
+                    }
+                }
+            }
+            first!! / second!!
+        }.sum())
     }
 }
